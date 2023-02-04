d4d105dde66c97b55829213911ceed43fb2b75cf
JunhuaL <linjunhua3@gmail.com>
2023-02-04 02:16:28 +0000

    Added Comments to the Class

--- bm25.py
+++ bm25.py
@@ -13,11 +13,41 @@ def get_Average_Document_Length():
     pass
 
 class BM25_Model():
+    """Class follows Okapi BM25 model for information retrieval which scores the documents
+    following a probabilistic model.  
+    """
     def __init__(self,positionalInvertedIndex,stopwords):
-        self.index = PositionalInvertedIndex()
+        """
+        Parameters
+        ----------
+        positionalInvertedIndex: PositionalInvertedIndex
+            An instance of the inverted index which provides an API that allows for 
+            retrieval of term-document information.
+        stopwords: List[String]
+            A list of common english words that a removed from the documents and a
+            query.
+        """
+        self.index = positionalInvertedIndex
         self.stopwords = stopwords
 
     def preprocess_query(self,query,removeStopWords=True,stem=True):
+        """Processes a set of terms by performing case folding, stopword removal and
+        stemming.
+        Parameters
+        ----------
+        query: String
+            The query given by the user
+        removeStopWords: bool, default True
+            Whether to remove stopwords from the given query
+            If True, commonly used english words are removed from the users query
+        stem: bool, default True
+            Whether to apply Porter stemmer to the words.
+            If True, words are stemmed to their roots.
+        Returns
+        ----------
+        terms: List[String]
+            A List of processed words after case folding, stopword removal and stemming.
+        """
         preprocessor = Preprocessor(query,self.stopwords)
         preprocessor.normaliseCases()
         if removeStopWords:
@@ -26,12 +56,26 @@ class BM25_Model():
             preprocessor.stemTerms()
         if len(preprocessor.terms) == 0:
             return ""
-        return preprocessor.terms[0]
+        return preprocessor.terms
 
     def score_document(self,query,doc_nos):
+        """Calculates the BM25 score of a batch of documents given a query.
+        Parameters
+        ----------
+        query: List[String]
+            List of terms after preprocessing is applied to a users query
+        doc_nos: List[int]
+            List of document IDs to calculate the score for.
+        Returns
+        ----------
+        results: List[(int,float)]
+            List of document ID and score pairs which indicate the relevancy of a
+            document to the query. 
+
+        """
+        k = 1.5
         # TODO: k is a hyperparameter which can be tuned later on. Currently using best
         # practise value given from the lectures.
-        k = 1.5
         results = []
         for doc_no in doc_nos:
             doc_score = 0
@@ -48,6 +92,18 @@ class BM25_Model():
         return results
     
     def partition_data(self,data_idxs,no_of_partitions):
+        """Helper function to partition data into chunks for preprocessing.
+        Parameters
+        -----------
+        data_idxs: List[int]
+            List of data identifiers, e.g document IDs
+        no_of_partitions: int
+            Number of partitions to split the data into
+        Returns
+        -----------
+        List[List[int]]
+            A list of lists containing document IDs
+        """
         splits = []
         data_size = len(data_idxs)
         chunk_size = data_size // no_of_partitions
@@ -61,10 +117,22 @@ class BM25_Model():
         return splits
 
     def ranked_search(self,query):
+        """Ranks documents in the index given a query from the user.
+        Parameters
+        ----------
+        query: String
+            The query string submitted by the user.
+        Returns
+        ----------
+        ranked_docs: List[(int,float)]
+            A list of all document ID and score pairs that indicate the relevancy
+            of a document to a user query. 
+        """
         query_terms = self.preprocess_query(query)
         doc_IDs = list(self.index.documentIDs)
         no_of_processes = mp.cpu_count() - (mp.cpu_count()//4)
 
+        # Using multiprocessing to rank documents in parallel
         doc_partitions = self.partition_data(doc_IDs,no_of_processes)
         pool = mp.Pool(processes=no_of_processes)
         results = [pool.apply_async(self.score_document,(query_terms,doc_IDs,)) for doc_IDs in doc_partitions]
