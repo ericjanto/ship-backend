5de15d9edb747cb4032f8313959b37c3a245bcc3
Eric Janto <e.janto@gmx.de>
2023-02-18 16:59:04 +0000

    Add autocompleter

---
+++ Autocompleter.py
@@ -0,0 +1,88 @@
+from rich.progress import Progress
+
+from PrefixTree import PrefixTree
+from TagPositionalInvertedIndexLoader import TagPositionalInvertedIndexLoader
+
+
+class Autocompleter:
+    """
+    Provides prefix autocomplete for the keys of a PI-index.
+
+    We use it for tag autocompletion.
+
+    In theory, we could autocomplete other index keys as well,
+    but they are likely to be preprocessed and would offer less value
+    to an end-user.
+    """
+    def __init__(self, path_to_compressed_tag_index: str) -> None:
+        self.pt = None
+        self.tag_index = None
+
+        # Update index and pt
+        self.build_prefix_tree(path_to_compressed_tag_index)
+
+    def get_ranked_autocomplete(self, prefix: str, n: int):
+        """
+        Rank by freq and then lexical string order
+
+        NOTE: sorting for every request might be too expensive for cases
+        where there are many leaves which share the same prefix?
+        """
+        completed_tags = self.pt.autocomplete(prefix)
+
+        # k[0] = frequency of tag, k[1] is tag itself
+        key_sort = lambda k: (k[1], Reversor(k[0]))
+        sorted_results_n = sorted(completed_tags, key=key_sort, reverse=True)[:n]
+
+        return sorted_results_n
+
+    def build_prefix_tree(self, path_to_compressed_tag_index: str) -> None:
+        """
+        - Decompress tag index
+        - Obtain tags by looking at keys
+        - Build tree with tags
+        - Update self.tag_index and self.pt
+
+        NOTE: re:performance, this takes ~15s on an M2 machine, with an index
+            containing 1491127 tags. This might not be efficient enough / we
+            might want to only do this once.
+        """
+        print("Decompressing ")
+        self.tag_index = TagPositionalInvertedIndexLoader.loadFromCompressedFile(
+            path_to_compressed_tag_index
+        )
+        tags = list(self.tag_index.tags.keys())
+        self.pt = PrefixTree()
+
+        with Progress() as p:
+            task = p.add_task("Building tree...", total=len(tags))
+            for t in tags:
+                f = self.tag_index.getTagFrequency(t)
+                self.pt.insert(t, f)
+                p.advance(task)
+
+
+class Reversor:
+    """
+    Used for being able to sort by two keys, one of them reversed.
+
+    See https://stackoverflow.com/a/56842689/.
+    """
+
+    def __init__(self, obj):
+        self.obj = obj
+
+    def __eq__(self, other):
+        return other.obj == self.obj
+
+    def __lt__(self, other):
+        return other.obj < self.obj
+
+
+if __name__ == '__main__':
+    PATH_TAG_INDEX = 'data/compressedTagIndex.bin'
+    autocompleter = Autocompleter(PATH_TAG_INDEX)
+
+    n = 5
+    print(autocompleter.get_ranked_autocomplete('harry', n))
+    print(autocompleter.get_ranked_autocomplete('l', n))
---
+++ PrefixTree.py
@@ -0,0 +1,86 @@
+from typing import List, Tuple
+
+
+class TrieNode:
+    """
+    Each node holds a text bit which is either
+    a tag (is_tag) or a prefix of a tag (!is_tag).
+
+    freq keeps track of how often a tag occurs in the index,
+    for ranking purposes.
+
+    freq > 0 implies is_tag, but we keep it explicit for clarity.
+    """
+
+    def __init__(self, text: str = "", freq: int = 0):
+        self.text = text
+        self.children = {}
+        self.is_tag = False
+        self.freq = freq
+
+
+class PrefixTree:
+    """
+    Tree data structure which we use for locating tags which have
+    a given prefix ("autocomplete").
+
+    For more information:
+        - https://en.wikipedia.org/wiki/Trie
+    """
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, tag: str, freq: int) -> None:
+        """
+        Inserts a new tag into the tree, along with its frequency
+        of occuring in the index.
+        """
+        current = self.root
+        for i, char in enumerate(tag):
+            if char not in current.children:
+                prefix = tag[0 : i + 1]
+                current.children[char] = TrieNode(prefix)
+            current = current.children[char]
+        current.is_tag = True
+        current.freq = freq
+
+    def find(self, tag: str) -> TrieNode | None:
+        """
+        Finds the TrieNode representing the given tag if it exists.
+        Returns None if not found.
+        """
+        current = self.root
+        for char in tag:
+            if char not in current.children:
+                return None
+            current = current.children[char]
+
+        if current.is_word:
+            return current
+
+    def autocomplete(self, prefix: str) -> List[str]:
+        """
+        Returns a list of all words beginning with the given prefix, or
+        an empty list if no words begin with that prefix.
+        """
+        tags = []
+        current = self.root
+        for char in prefix:
+            if char not in current.children:
+                return []
+            current = current.children[char]
+
+        self.__child_words_for(current, tags)
+        return tags
+
+    def __child_words_for(self, node: TrieNode, tags: List[Tuple[str, int]]):
+        """
+        Cycles through all children
+        of node recursively, adding them to words if they
+        constitute whole words (as opposed to merely prefixes).
+        """
+        if node.is_tag:
+            tags.append((node.text, node.freq))
+        for letter in node.children:
+            self.__child_words_for(node.children[letter], tags)
