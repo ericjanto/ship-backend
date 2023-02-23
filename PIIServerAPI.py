40a1be84c7cdb7e96886bea6c2fd3595f43a5b3e
matthewski490@yahoo.co.uk <MatthewWisniewski>
2023-02-23 13:48:42 +0000

    Refactored out the json parse / index operation / json wrap into its own method, so that it is a different level of abstraction from sending back the response to the client

--- PIIServerAPI.py
+++ PIIServerAPI.py
@@ -14,47 +14,63 @@ class PIIServerAPI:
         self.initialiseSocket(ip, socket)
         self.clientId = 0
         self.listen()
-    
-    def process_query(self, message, addr):
-        """ Process a query from a client, where message is of types bytes and needs to be converted to a json object """
+
+    def process_request(self, message, addr):
+
+        response = self.process_query(message)
+
+        self.recvSocket.sendto(json.dumps(response).encode(), addr)
+
+    def process_query(self, message):
+        """
+        Takes the JSON request received from the client,
+        runs the requested operation against the index, and wraps
+        the result in another JSON object.
+        """
         json_recv = json.loads(message)
         method = json_recv["method"]
         data = {}
-        if method=="requestClientId":
+
+        if method == "requestClientId":
             data["clientId"] = self.clientId
             self.clientId += 1
 
         elif method == "getDistinctTermsCount":
-            data[method] = self.index.getDistinctTermsCount()
+            data["distinctTermCount"] = self.index.getDistinctTermsCount()
 
         elif method == "getEnglishTermsCount":
-            data[method] = self.index.getEnglishTermsCount()
+            data["englishTermCount"] = self.index.getEnglishTermsCount()
 
         elif method == "getTermFrequency":
             pairs = json_recv["pairs"]
             for term, docID in pairs:
-                data[(method, term, docID)] = self.index.getTermFrequency(term, docID)
+                data[(term, docID)] = self.index.getTermFrequency(term, docID)
 
         elif method == "getDocFrequency":
             terms = json_recv["terms"]
             for term in terms:
-                data[(method, term)] = self.index.getDocFrequency(term)
+                data[term] = self.index.getDocFrequency(term)
 
         elif method == "getDocumentsTermOccursIn":
             terms = json_recv["terms"]
             for term in terms:
-                data[(method,term)] = self.index.getDocumentsTermOccursIn(term)
+                data[term] = self.index.getDocumentsTermOccursIn(term)
 
         elif method == "getPostingList":
             pairs = json_recv["pairs"]
             for (term, docID) in pairs:
-                data[(method, term, docID)] = self.index.getPostingList(term, docID)
+                data[(term, docID)] = self.index.getPostingList(term, docID)
 
         else:
             print("Error: invalid method")
-            data = None
-        response = {"request_id": request_id, "client_id":client_id, "data": data}
-        self.recvSocket.sendto(json.dumps(response).encode(), addr)
+            # We need to return something if an invalid request is
+            # sent, else the client will never stop waiting for a response.
+            data["invalidQueryType"] = True
+#        response = {"request_id": request_id, "client_id":client_id, "data": data}
+        data["requestID"] = json_recv["requestID"]
+        data["clientID"] = json_recv["clientID"]
+
+        return data
 
     def initialiseSocket(self, ipAddr, socketNo):
         self.recvSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -64,4 +80,4 @@ class PIIServerAPI:
         self.recvSocket.listen(5)
         while True:
             message, addr = client.recv(4096)
-            threading.Thread(target=process_query, args=(message, addr)).start()
+            threading.Thread(target=process_request, args=(message, addr)).start()
