484dbb9a520fed5a00d10cc27ea6c83a178da6cf
MrinmoySonowal <M.Sonowal@sms.ed.ac.uk>
2023-03-16 17:55:02 +0000

    Added autocomplete functionality

--- TagPIIClientFastAPI.py
+++ TagPIIClientFastAPI.py
@@ -10,6 +10,11 @@ class TagPIIClientFastAPI:
         self.ip = ip
         self.port = port
 
+    def get_ranked_autocomplete(self, pairs):
+        data = {'pairs': pairs}
+        response = requests.post(f'http://{self.ip}:{self.port}/autocomplete', json=data)
+        return response.json()
+
     def getStoryIDsWithTag(self, tags):
         data = {'tags': tags}
         response = requests.post(f'http://{self.ip}:{self.port}/getStoryIDsWithTag', json=data)
@@ -20,8 +25,18 @@ class TagPIIClientFastAPI:
         response = requests.post(f'http://{self.ip}:{self.port}/getTagFrequency', json=data)
         return response.json()
 
+    def mergeWithOtherIndex(self, dateFileNames):
+        data = {'dateFileNames': dateFileNames}
+        response = requests.put(f'http://{self.ip}:{self.port}/mergeWithOtherIndex', json=data)
+        return response.json()
+    
+    def mergeWithOtherIndexAllDates(self):
+        response = requests.put(f'http://{self.ip}:{self.port}/mergeWithOtherIndexAllDates')
+        return response.json()
+
 if __name__ == '__main__':
     client = TagPIIClientFastAPI('localhost', 5002)
     tag_results = client.getStoryIDsWithTag(['anime', 'manga'])
     print(set(tag_results['anime']).intersection(set(tag_results['manga'])))
     print(client.getTagFrequency(['marvel', 'magic']))
+    print(client.mergeWithOtherIndex(['compressedTagIndexFull.bin']))
--- TagPIIServerFastAPI.py
+++ TagPIIServerFastAPI.py
@@ -6,15 +6,19 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from TagPositionalInvertedIndexLoader import TagPositionalInvertedIndexLoader
 import uvicorn
+import os
 
 app = FastAPI()
 index = None
+autocompleter = None
 
 @app.on_event("startup")
 async def startup_event():
     global index
     global indexFile
-    index = TagPositionalInvertedIndexLoader.loadFromCompressedFile(indexFile)
+    global autocompleter
+    autocompleter = Autocompleter(indexFile)
+    index = autocompleter.tag_index
     print("Index loaded")
 
 @app.get("/test")
@@ -24,12 +28,24 @@ def test(request: Request):
 
 @app.post("/getStoryIDsWithTag")
 async def getStoryIDsWithTag(request: Request):
-    tags = await request.body()
+    tags = await request.body`()
     tags = json.loads(tags)["tags"]
     response = {}
     for tag in tags:
         response[tag] = index.getStoryIDsWithTag(tag)
     return JSONResponse(content=response, status_code=200)
+
+@app.post("/autocomplete")
+def get_ranked_autocomplete(self, rrequest:Request):
+    pairs = request.body()
+    pairs = json.loads(pairs)["pairs"]
+    results = {}
+    for prefix, n in pairs:
+        if prefix not in results:
+            results[prefix] = {}
+        results[prefix][n] = self.autocompleter.autocomplete(prefix, n)
+    return JSONResponse(content=results, status_code=200)
+
 
 @app.post("/getTagFrequency")
 async def getTagFrequency(request: Request):
@@ -39,6 +55,29 @@ async def getTagFrequency(request: Request):
     for tag in tags:
         response[tag] = index.getTagFrequency(tag)
     return JSONResponse(content=response, status_code=200)
+
+@app.put("/mergeWithOtherIndex")
+async def mergeWithOtherIndex(request: Request):
+    global index
+    dateFileNames = await request.body()
+    dateFileNames = json.loads(dateFileNames, parse_int=int)["dateFileNames"]
+    for dateFileName in dateFileNames:
+        # path = "./data/WebScraperImports/WebScraped-Tags/" + dateFileName
+        path = "./data/" + dateFileName
+        if os.path.exists(path):
+            tagIndex = TagPositionalInvertedIndexLoader.loadFromCompressedFile(path)
+            index.mergeWithOtherIndex(tagIndex)
+    return JSONResponse(content="Done Merging!", status_code=200)
+
+@app.put("/mergeWithOtherIndexAllDates")
+async def mergeWithOtherIndexAllDates():
+    global index
+    path = "./data/WebScraperImports/WebScraped-Tags/"
+    # list all files in the directory
+    for fileName in os.listdir(path):
+        tagIndex = TagPositionalInvertedIndexLoader.loadFromCompressedFile(path+fileName)
+        index.mergeWithOtherIndex(tagIndex)
+    return JSONResponse(content="Done Merging!", status_code=200)
 
 if __name__ == '__main__':
     global indexFile
