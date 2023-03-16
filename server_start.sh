f1de5a3026c40ea95ddf5c235cdd2a7a3bdb9b1f
Clevershelf009 <matthewski490@yahoo.co.uk>
2023-03-16 15:42:47 +0000

    Modified server start script to run with full dataset

--- server_start.sh
+++ server_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3.9 PIIServerFastAPI.py localhost 5001 data/chapters-index-vbytes.bin &
-python3.9 TagPIIServerFastAPI.py localhost 5002 data/compressedTagIndex.bin &
-python3.9 TermCountsServer.py localhost 5003 data/termCounts.bin &
-python3.9 StoryMetadataServer.py localhost 5004 data/compressedMetadata.bin
+python3.9 PIIServerFastAPI.py localhost 5001 data/chapters-indexes-emergency-query/ y &
+python3.9 TagPIIServerFastAPI.py localhost 5002 data/compressedTagIndexFull.bin &
+python3.9 TermCountsServer.py localhost 5003 data/termCountsFull.bin &
+python3.9 StoryMetadataServer.py localhost 5004 data/compressedMetadataFull.bin
