02abf916744f10d933e90502e4a73fb937228db2
Clevershelf009 <matthewski490@yahoo.co.uk>
2023-03-16 17:01:25 +0000

    update ips in server start

--- server_start.sh
+++ server_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3.9 PIIServerFastAPI.py localhost 5001 data/compressed-chapter-indexes-emergency-query/ y &
-python3.9 TagPIIServerFastAPI.py localhost 5002 data/compressedTagIndexFull.bin &
-python3.9 TermCountsServer.py localhost 5003 data/termCountsFull.bin &
-python3.9 StoryMetadataServer.py localhost 5004 data/compressedMetadataFull.bin
+python3.9 PIIServerFastAPI.py 10.132.0.4 5001 data/compressed-chapter-indexes-emergency-query/ y &
+python3.9 TagPIIServerFastAPI.py 10.132.0.4 5002 data/compressedTagIndexFull.bin &
+python3.9 TermCountsServer.py 10.132.0.4 5003 data/termCountsFull.bin &
+python3.9 StoryMetadataServer.py 10.132.0.4 5004 data/compressedMetadataFull.bin
