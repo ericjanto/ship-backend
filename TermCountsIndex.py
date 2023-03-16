9978b3612256e84cfa057798545cff93cea8e86d
JunhuaL <linjunhua3@gmail.com>
2023-03-16 22:07:53 +0000

    syncing local repo

--- TermCountsIndex.py
+++ TermCountsIndex.py
@@ -15,42 +15,42 @@ class TermCountsIndex:
         if type(docID) == str:
             docID = int(docID)
         if docID not in self.index:
-            return []
+            return 0
         return self.index[docID][0]
     
     def get_tokens_before_stemming(self, docID):
         if type(docID) == str:
             docID = int(docID)
         if docID not in self.index:
-            return []
+            return 0
         return self.index[docID][1]
     
     def get_unique_tokens_before_processing(self, docID):
         if type(docID) == str:
             docID = int(docID)
         if docID not in self.index:
-            return []
+            return 0
         return self.index[docID][2]
     
     def get_unique_tokens_before_stemming(self, docID):
         if type(docID) == str:
             docID = int(docID)
         if docID not in self.index:
-            return []
+            return 0
         return self.index[docID][3]
     
     def get_tokens_after_stemming(self, docID):
         if type(docID) == str:
             docID = int(docID)
         if docID not in self.index:
-            return []
+            return 0
         return self.index[docID][4]
     
     def get_all_term_counts_for(self, docID):
         if type(docID) == str:
             docID = int(docID)
         if docID not in self.index:
-            return []
+            return 0
         return self.index[docID]
 
     def get_all_term_counts(self):
